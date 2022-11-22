c8e00783bc05d686788d26a87b5ed6d0f1c5ec83
Johannes Thyroff <johannes.thyroff@gmail.com>
2022-11-22 16:01:43 +0100

    Add example code

---
+++ src/Controller/Admin/IndexController.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\MiraklConnector\Controller\Admin;
+
+use PrestaShop\Module\MiraklConnector\Grid\Definition\Factory\ProductGridDefinitionFactory;
+use Prestashop\Module\MiraklConnector\Grid\Filters\ProductFilters;
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use PrestaShopBundle\Service\Grid\ResponseBuilder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends FrameworkBundleAdminController
+{
+    /**
+     * List quotes
+     *
+     * @param ProductFilters $filters
+     *
+     * @return Response
+     */
+    public function indexAction(ProductFilters $filters)
+    {
+        $quoteGridFactory = $this->get('mirakl_connector.grid.factory.products');
+        $quoteGrid = $quoteGridFactory->getGrid($filters);
+
+        return $this->render(
+            '@Modules/miraklconnector/views/templates/admin/index.html.twig',
+            [
+                'enableSidebar' => true,
+                'layoutTitle' => $this->trans('Product listing', 'Modules.Miraklconnector.Admin'),
+                'quoteGrid' => $this->presentGrid($quoteGrid),
+            ]
+        );
+    }
+
+    /**
+     * Provides filters functionality.
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function searchAction(Request $request)
+    {
+        /** @var ResponseBuilder $responseBuilder */
+        $responseBuilder = $this->get('prestashop.bundle.grid.response_builder');
+
+        return $responseBuilder->buildSearchResponse(
+            $this->get('mirakl_connector.grid.definition.factory.products'),
+            $request,
+            ProductGridDefinitionFactory::GRID_ID,
+            'mirakl_connector_index'
+        );
+    }
+}
