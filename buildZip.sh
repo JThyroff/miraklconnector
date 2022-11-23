6ff1fcef4eee9ecff60bdfcd40aeab83d3ffd3ef
Johannes Thyroff <johannes.thyroff@gmail.com>
2022-11-23 12:43:17 +0100

    Improve build script

--- buildZip.sh
+++ buildZip.sh
@@ -1,1 +1,8 @@
-zip miraklconnector.zip *
+MODULE='miraklconnector'
+ARCHIVE=$MODULE'.zip'
+
+cd ..
+
+zip -r $ARCHIVE $MODULE -x $MODULE'/.git/*' -x $MODULE'/js/node_modules/*'
+
+mv $ARCHIVE $MODULE
