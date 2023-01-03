94ac61c58fb37b333b30efcd5df45e381bc8fc47
Johannes Thyroff <johannes.thyroff@gmail.com>
2023-01-03 11:50:05 +0100

    Order insertion

--- src/Mirakl/MiraklDatabase.php
+++ src/Mirakl/MiraklDatabase.php
@@ -68,11 +68,21 @@ class MiraklDatabase
         return $conn;
     }
 
-    public static function insertOrder(mysqli $conn, array $order){
+    /**
+     * @param mysqli $conn database connection
+     * @param array $order prepared order array from GridPrepare::extractInvoiceFields()
+     * @return int ID of the new entry
+     */
+    public static function insertBillingAddress(mysqli $conn, array $order): int
+    {
         $billingAddr = $order['billingAddress'];
 
-        $stmt = $conn->prepare("INSERT INTO `BillingAddress` (`ID`, `city`, `civility`, `country`, `firstname`, `lastname`, `phone`, `state`, `street`, `zip_code`) 
-            VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+        $stmt = $conn->prepare(
+            "INSERT INTO `BillingAddress` (
+                `ID`, `city`, `civility`, `country`, `firstname`, `lastname`, `phone`, `state`, `street`, `zip_code`
+            ) 
+            VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+        );
 
         $city = $billingAddr->getCity();
         $civility = $billingAddr->getCivility();
@@ -83,7 +93,7 @@ class MiraklDatabase
         $state = $billingAddr->getState();
         $street1 = $billingAddr->getStreet1();
         $zipCode = $billingAddr->getZipCode();
-        
+
         $stmt->bind_param('sssssssss',
             $city, $civility, $country, $firstname, $lastname, $phone, $state, $street1, $zipCode
         );
@@ -92,9 +102,57 @@ class MiraklDatabase
             if ($stmt->execute()) {
                 echo("BillingAddress : Insertion successfully.\n");
             }
-        }catch (mysqli_sql_exception $e){
-            echo $e->getMessage()."\n";
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
+            return -1;
         }
+        $sql = "SELECT LAST_INSERT_ID();";
+
+        return $conn->query($sql)->fetch_row()[0];
+    }
+
+    public static function insertOrder(mysqli $conn, array $order)
+    {
+        $billingAddress = MiraklDatabase::insertBillingAddress($conn, $order);
+
+        #INSERT INTO `Orders` (`date`, `billingAddress`, `title`, `sku`, `quantity`, `basePricePerUnit`, `basePrice`, `totalBasePrice`, `taxes`, `commissionTaxRate`, `shippingPrice`, `shippingTaxes`, `totalPrice`) VALUES ('2022-05-12 08:14:35.000000', '1', 'Bresser Arcturus Telescopio Refractor 60/700 AZ con Filtro Solar y Adaptador para Móvil', '267523', '2', '92.55', '185.1', '189.19', '38.88', '21.00', '4.09', '0.86', '228.93');
+
+        $stmt = $conn->prepare(
+            "INSERT INTO `Orders` (
+                      `date`, `billingAddress`, `title`, `sku`, `quantity`, `basePricePerUnit`, `basePrice`
+                      , `totalBasePrice`, `taxes`, `commissionTaxRate`, `shippingPrice`, `shippingTaxes`, `totalPrice`
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+        );
+
+        $date = $order['date']->format('Y-m-d H:i:s');
+        $title = $order['title'];
+        $sku = $order['sku'];
+        $quantity = $order['quantity'];
+        $basePricePerUnit = $order['basePricePerUnit'];
+        $basePrice = $order['basePrice'];
+        $totalBasePrice = $order['totalBasePrice'];
+        $taxAmount = $order['taxes']->first()->getAmount();
+        $commissionTaxRate = $order['commissionTaxRate'];
+        $shippingPrice = $order['shippingPrice'];
+        $shippingTaxAmount = $order['shippingTaxes']->first()->getAmount();
+        $totalPrice = $order['totalPrice'];
+
+        $stmt->bind_param('sisiidddddddd',
+            $date, $billingAddress, $title, $sku, $quantity, $basePricePerUnit, $basePrice, $totalBasePrice
+            , $taxAmount, $commissionTaxRate, $shippingPrice, $shippingTaxAmount, $totalPrice
+        );
+
+        try {
+            if ($stmt->execute()) {
+                echo("Order : Insertion successfully.\n");
+            }
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
+            return -1;
+        }
+        $sql = "SELECT LAST_INSERT_ID();";
+
+        return $conn->query($sql)->fetch_row()[0];
     }
 
     public static function createTables(mysqli $conn): void
@@ -105,17 +163,8 @@ class MiraklDatabase
             if ($conn->query($sql)) {
                 echo("BillingAddress : Table created successfully.\n");
             }
-        }catch (mysqli_sql_exception $e){
-            echo $e->getMessage()."\n";
-        }
-
-        $sql = "CREATE TABLE `PC_CompOrders`.`Orders` (`date` DATETIME NOT NULL , `billingAddress` INT NOT NULL , `title` VARCHAR(255) NOT NULL , `sku` INT NOT NULL , `quantity` INT NOT NULL , `basePricePerUnit` FLOAT NOT NULL , `basePrice` FLOAT NOT NULL , `totalBasePrice` FLOAT NOT NULL , `taxes` FLOAT NOT NULL , `commissionTaxRate` FLOAT NOT NULL , `shippingPrice` FLOAT NOT NULL , `shippingTaxes` FLOAT NOT NULL , `totalPrice` FLOAT NOT NULL ) ENGINE = InnoDB;";
-        try {
-            if ($conn->query($sql)) {
-                echo("Orders : Table created successfully.\n");
-            }
-        }catch (mysqli_sql_exception $e){
-            echo $e->getMessage()."\n";
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
         }
 
         $sql = "ALTER TABLE `BillingAddress` ADD PRIMARY KEY(`ID`);";
@@ -123,8 +172,8 @@ class MiraklDatabase
             if ($conn->query($sql)) {
                 echo("BillingAddress : Set ID as primary key successfully.\n");
             }
-        }catch (mysqli_sql_exception $e){
-            echo $e->getMessage()."\n";
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
         }
 
         $sql = "ALTER TABLE `BillingAddress` CHANGE `ID` `ID` INT(11) NOT NULL AUTO_INCREMENT;";
@@ -132,17 +181,35 @@ class MiraklDatabase
             if ($conn->query($sql)) {
                 echo("BillingAddress : Set ID to auto increment successfully.\n");
             }
-        }catch (mysqli_sql_exception $e){
-            echo $e->getMessage()."\n";
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
         }
+
+        $sql = "CREATE TABLE `PC_CompOrders`.`Orders` (
+            `date` DATETIME NOT NULL , `billingAddress` INT NOT NULL , `title` VARCHAR(255) NOT NULL 
+            , `sku` INT NOT NULL , `quantity` INT NOT NULL , `basePricePerUnit` FLOAT NOT NULL 
+            , `basePrice` FLOAT NOT NULL , `totalBasePrice` FLOAT NOT NULL , `taxes` FLOAT NOT NULL 
+            , `commissionTaxRate` FLOAT NOT NULL , `shippingPrice` FLOAT NOT NULL , `shippingTaxes` FLOAT NOT NULL 
+            , `totalPrice` FLOAT NOT NULL
+            , FOREIGN KEY (billingAddress) REFERENCES `PC_CompOrders`.`BillingAddress`(ID) ON DELETE CASCADE 
+        ) ENGINE = InnoDB;";
+
+        try {
+            if ($conn->query($sql)) {
+                echo("Orders : Table created successfully.\n");
+            }
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
+        }
+
 
         $sql = "ALTER TABLE `Orders` ADD PRIMARY KEY(`date`, `billingAddress`, `title`, `sku`, `quantity`);";
         try {
             if ($conn->query($sql)) {
                 echo("Orders : Set primary key successfully.\n");
             }
-        }catch (mysqli_sql_exception $e){
-            echo $e->getMessage()."\n";
+        } catch (mysqli_sql_exception $e) {
+            echo $e->getMessage() . "\n";
         }
         echo "Tables created.\n";
     }
@@ -157,7 +224,7 @@ $response = $client->getOrders($request);
 $invoiceFields = GridPrepare::extractInvoiceFields(...$response);
 foreach ($invoiceFields as $order) {
     var_dump($order);
-    MiraklDatabase::insertOrder($mysqli, $order);
+    var_dump(MiraklDatabase::insertOrder($mysqli, $order));
     break;
 }
 
